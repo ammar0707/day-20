6ce715f437474d70de57c152f6d0af9469723ade
ammar0707 <happyhappiest98@gmail.com>
2022-04-24 06:28:25 +0700

    first revisi

---
+++ CE12.js
@@ -0,0 +1,23 @@
+// Soal: 
+// Pada suatu toko terdapat beberapa kaos kaki
+// warna biru dinyatakan dengan 1
+// warna hijau dinyatakan dengan 3
+// warna hitam dinyatakan dengan 5
+// warna merah dinyatakan denga 8
+// koleksi kaos kaki pada toko dinyatakan dengan array [1, 1, 3, 3, 5, 5, 5, 5]
+// disebut sebagai sepasang kaos kaki apabila kaos kaki tersebut memiliki pasangan
+// contoh :
+// [1,3,3,3] => pada array ini, ada 1 kaos kaki biru, dan 3 kaos kaki hijau. Maka hasilnya ada sepasang(2) kaos kaki hijau dan masing2 1 kaos kaki biru dan 1 kaos kaki hijau lainnya yang tidak memiliki pasangan
+
+// Expected Output :
+// 4
+
+// Berapa pasang kaos kaki kah yang tersedia?
+// Tidak boleh langsung print expected output
+
+// Jawaban:
+function sockMerchant(word) {
+    // write your code here
+    }
+    
+    console.log(sockMerchant([1, 1, 3, 3, 5, 5, 5, 5]))
